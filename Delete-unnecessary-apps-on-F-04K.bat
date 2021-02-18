f921e6da335199f0c13a5b403cd3dcc8feec248c
sunset0916 <61398531+sunset0916@users.noreply.github.com>
2021-02-18 23:25:32 +0900

    Add files via upload
    add new file

---
+++ Delete-unnecessary-apps-on-F-04K.bat
@@ -0,0 +1,96 @@
+@echo off
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.applicationmanager
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.accountauthenticator
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.dhome
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.idmanager
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.store
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.dmenu2
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.iconcier
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.mascot
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.devicehelp
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.remotelock
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.bugreport
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.areamail
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.atf
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.iconcier_contents
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.phonemotion
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.databackup
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.schedulememo
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.cloudset
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.docomoset
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.sdcardbackup
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.voiceeditor
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.screenlockservice
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.pf.dcmwappush
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.pf.dcmippushaggregator
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.accountwipe
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.initialization
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.kittingmanager
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.wipe
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.epshome
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.epsmodecontrol
+adb shell pm uninstall -k --user 0 jp.co.nttdocomo.carriermail
+adb shell pm uninstall -k --user 0 jp.co.nttdocomo.lcsapp
+adb shell pm uninstall -k --user 0 jp.co.nttdocomo.lcsappsub
+adb shell pm uninstall -k --user 0 jp.co.nttdocomo.saigaiban
+adb shell pm uninstall -k --user 0 com.nextbit.app
+adb shell pm uninstall -k --user 0 jp.scn.android
+adb shell pm uninstall -k --user 0 jp.co.omronsoft.android.decoemojimanager_docomo
+adb shell pm uninstall -k --user 0 com.fujitsu.mobile_phone.nxmail
+adb shell pm uninstall -k --user 0 com.fujitsu.mobile_phone.fileshare
+adb shell pm uninstall -k --user 0 com.fujitsu.mobile_phone.screenmemo
+adb shell pm uninstall -k --user 0 com.fujitsu.mobile_phone.slowmovieeditor
+adb shell pm uninstall -k --user 0 com.fujitsu.mobile_phone.passwordmanager
+adb shell pm uninstall -k --user 0 com.fujitsu.mobile_phone.MusicPicker
+adb shell pm uninstall -k --user 0 com.fujitsu.mobile_phone.healthcare
+adb shell pm uninstall -k --user 0 com.fujitsu.mobile_phone.dataprotection
+adb shell pm uninstall -k --user 0 com.fujitsu.mobile_phone.dataprotection.ui
+adb shell pm uninstall -k --user 0 com.atok.mobile.im.fujitsu.theme.model.aq
+adb shell pm uninstall -k --user 0 com.atok.mobile.im.fujitsu.theme.model.el
+adb shell pm uninstall -k --user 0 com.atok.mobile.im.fujitsu.theme.model.fpd
+adb shell pm uninstall -k --user 0 com.atok.mobile.im.fujitsu.theme.model.ga
+adb shell pm uninstall -k --user 0 com.atok.mobile.im.fujitsu.theme.model.ip
+adb shell pm uninstall -k --user 0 com.atok.mobile.im.fujitsu.theme.model.po
+adb shell pm uninstall -k --user 0 com.atok.mobile.im.fujitsu.service
+adb shell pm uninstall -k --user 0 com.atok.mobile.im.fujitsu.dictionary
+adb shell pm uninstall -k --user 0 com.atok.mobile.im.fujitsu.theme.custom1
+adb shell pm uninstall -k --user 0 com.atok.mobile.im.fujitsu.theme.custom2
+adb shell pm uninstall -k --user 0 com.atok.mobile.im.fujitsu.theme.flicklearning
+adb shell pm uninstall -k --user 0 com.atok.mobile.im.fujitsu.theme.black
+adb shell pm uninstall -k --user 0 com.atok.mobile.im.fujitsu.theme.basic
+adb shell pm uninstall -k --user 0 com.atok.mobile.im.fujitsu.theme.white
+adb uninstall com.nttdocomo.android.mydocomo
+adb uninstall com.nttdocomo.android.applicationmanagersub
+adb uninstall com.nttdocomo.android.photocollection
+adb uninstall com.nttdocomo.android.dhealthcare
+adb uninstall com.nttdocomo.android.dpoint
+adb uninstall com.nttdocomo.keitai.payment
+adb uninstall com.nttdocomo.android.anshinsecurity
+adb uninstall com.nttdocomo.android.voicetranslation
+adb uninstall com.nttdocomo.android.cloudstorageservice
+adb uninstall com.nttdocomo.android.toruca
+adb uninstall com.nttdocomo.android.moneyrecord
+adb uninstall com.nttdocomo.android.msg
+adb uninstall jp.co.nttdocomo.dbook
+adb uninstall jp.buffalo.aoss
+adb uninstall com.amazon.kindle
+adb uninstall com.amazon.mShop.android.shopping
+adb uninstall com.microsoft.office.excel
+adb uninstall com.microsoft.office.onenote
+adb uninstall com.microsoft.office.powerpoint
+adb uninstall com.microsoft.office.word
+adb uninstall com.microsoft.skydrive
+adb uninstall com.facebook.katana
+adb uninstall com.facebook.orca
+adb uninstall com.ipg.gguide.dcm_app.android
+adb uninstall com.onkyo.jp.musicplayer
+adb uninstall com.instagram.android
+adb uninstall com.twitter.android
+adb uninstall jp.id_credit_sp.android
+adb uninstall jp.co.mcdonalds.android
+adb uninstall jp.co.lawson.android
+adb uninstall com.fujitsu.mobile_phone.manual.f04k
+adb uninstall com.fujitsu.mobile_phone.Araikata
+adb uninstall com.fujitsu.mobile_phone.atfsite
+adb uninstall jp.dmapnavi.navi02
+adb uninstall com.rsupport.rs.activity.ntt
